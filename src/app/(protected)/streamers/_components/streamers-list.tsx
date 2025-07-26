c1742a3d578292accb88afa044524e8ff25301ce
Jacob <jacoblpbiz@gmail.com>
2025-07-26 14:58:32 +0100

    Update streamers-list.tsx

--- src/app/(protected)/streamers/_components/streamers-list.tsx
+++ src/app/(protected)/streamers/_components/streamers-list.tsx
@@ -236,7 +236,7 @@ export default function StreamersList({ initialStreamers, userId }: StreamersLis
         </div>
 
         {/* Live Streamers Grid */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4">
           {streamers
             .filter(streamer => {
               switch (streamFilter) {
